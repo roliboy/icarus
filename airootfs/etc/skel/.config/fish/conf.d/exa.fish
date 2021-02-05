3bcdc6dc07898c57c57be099b245b7945c0ce7a4
roliboy <roliboy@protonmail.com>
2021-02-05 15:01:35 +0200

    bpython and exa color scheme

---
+++ airootfs/etc/skel/.config/fish/conf.d/exa.fish
@@ -0,0 +1,81 @@
+set fish_greeting
+
+
+set -x EXA_COLORS "di=1;36:flag=4;32"
+
+#fi Normal file
+#di Directory
+#ex Executable file
+#pi Named pipe
+#so Socket
+#bd Block device
+#cd Character device
+#ln Symlink
+#or Broken symlink
+#PERMISSIONS
+#ur User +r bit
+#uw User +w bit
+#ux User +x bit (files)
+#ue User +x bit (file types)
+#gr Group +r bit
+#gw Group +w bit
+#gx Group +x bit
+#tr Others +r bit
+#tw Others +w bit
+#tx Others +x bit
+#su Higher bits (files)
+#sf Higher bits (other types)
+#xa Extended attribute marker
+#FILE SIZES
+#sn Size numbers
+#sb Size unit
+#df Major device ID
+#ds Minor device ID
+#OWNERS AND GROUPS
+#uu A user that’s you
+#un A user that’s not
+#gu A group with you in it
+#gn A group without you
+#HARD LINKS
+#lc Number of links
+#lm A multi-link file
+#GIT
+#ga New
+#gm Modified
+#gd Deleted
+#gv Renamed
+#gt Type change
+#DETAILS AND METADATA
+#xx Punctuation
+#da Timestamp
+#in File inode
+#bl Number of blocks
+#hd Table header row
+#lp Symlink path
+#cc Control character
+#OVERLAYS
+#bO Broken link path
+#FOREGROUND
+#30 Black
+#31 Red
+#32 Green
+#33 Yellow
+#34 Blue
+#35 Purple
+#36 Cyan
+#37 White
+#BACKGROUND
+#40 On black
+#41 On red
+#42 On green
+#43 On yellow
+#44 On blue
+#45 On purple
+#46 On cyan
+#47 On white
+#EFFECTS
+#01 Bold
+#04 Underline
+#256 COLOURS
+#38;5;nnn Foreground
+#48;5;nnn Background
